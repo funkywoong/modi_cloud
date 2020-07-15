968233a1954b42bb9d5e9891fee87c39d7eb5037
funkywoong <duswldnd12@naver.com>
2020-07-15 12:30:01 +0900

    Test server-client communication

---
+++ modi_cloud/client_test.py
@@ -0,0 +1,63 @@
+import logging
+import grpc
+import h5py
+import numpy as np
+import numpy as np
+
+from __future__ import print_function
+from io import BytesIO 
+from joblib import load, dump
+
+from modi_cloud.util.modi_ai_cloud_pb2.py
+from modi_cloud.util.modi_ai_cloud_pb2_grpc.py
+
+def __parse_data(target):
+    if isinstance(target, bytes):
+        return target
+    buf = BytesIO()
+    if isinstance(data, np.ndarray):
+        np.save(buf, target, allow_pickle=True)
+    elif 'keras' in str(type(target)):
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+        from tensorflow.keras.models import save_model
+        with h5py.File(buf, 'w') as f:
+            save_model(target, f, include_optimizer=True)
+    elif 'sklearn' in str(type(target)):
+        dump(target, buf)
+    buf.seek(0)
+    
+    return buf.read()
+
+# def serialize(target):
+#     buf = BytesIO()
+#     if isinstance(target, np.ndarray):
+#         np.save(buf, target, allow_pickle=True)
+    
+#     buf.seek(0)
+#     return buf.read()
+
+def run():
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    tmp_train_list = np.array([1, 2, 3, 4])
+    tmp_label_list = np.array([4, 5, 6, 7])
+    tmp_train_list = __parse_data(tmp_train_list)
+    tmp_label_list = __parse_data(tmp_label_list)
+    tmp_model = None
+
+    with grpc.insecure_channel('ec2-15-164-216-238.ap-northeast-2.compute.amazonaws.com:8000') as channel:
+        client_stub = modi_ai_cloud_pb2_grpc.Data_Model_HandlerStub(channel)
+        response_transfer = client_stub.SendObjects(
+            modi_ai_cloud_pb2.TransferCompleteSend(ask_transfer=1)
+        )
+        response_train = client_stub.SendObjects(
+            modi_ai_cloud_pb2.ObjectsSend(
+                target_array=tmp_train_list, label_arrary=tmp_label_list, model=tmp_model)
+        )
+
+    print("Label handler client received: ", response_transfer.reply_transfer)
+ 
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
---
+++ modi_cloud/modi_cloud.py
@@ -0,0 +1,103 @@
+import time
+import logging
+import grpc
+import h5py
+import numpy as np
+import threading as th
+
+from joblib import load, dump
+from concurrent import futures
+from io import BytesIO
+
+from modi_cloud.util.modi_ai_cloud_pb2.py
+from modi_cloud.util.modi_ai_cloud_pb2_grpc.py
+
+class Data_model_handler(modi_ai_cloud_pb2_grpc.Data_Model_HandlerServicer):
+
+    def __init__(self):
+        super().__init__()
+
+        self.__trns_flag = th.Event()
+
+    def SendObjects(self, request, context):
+
+        train_data = __load_data(request.train_array)
+        label_data = __load_data(request.label_array)
+        model = __load_data(request.model)
+
+        if not __is_transfer_ok(train_data, label_data, model):
+            self.__trns_flag.set()
+
+        tmp_reply = bytes(b'complete')
+        print('bye')
+        return modi_ai_cloud_pb2.ModelReply(trained_model=tmp_reply)
+
+    def TransferComplete(self, request, context):
+        if request.ask_transfer:
+            self.__trns_flag.wait()
+
+        return modi_ai_cloud_pb2.TransferCompleteReply(reply_transfer=-1)
+
+            
+
+        #TODO: training data by using given model
+
+    def __is_transfer_ok(self, train_data, label_data, model):
+        return(
+            train_data is not None and
+            label_data is not None and
+            model is not None
+        )
+
+    def __load_data(self, target):
+        buf = BytesIO()
+        buf.write(target)
+        buf.seek(0)
+        data_type = buf.read()[:-3]
+        buf.seek(0)
+        if b'NUMPY' in data_type:
+            return np.load(buf)
+        elif b'sklearn' in data_type:
+            return load(target)
+        elif b'HDF' in data_type:
+            os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+            from tensorflow.keras.models import load_model
+            with h5py.File(target, 'r') as f:
+                model = load_model(f)
+            return model
+        else:
+            return None
+
+        
+
+    # def SendArray(self, request, context):
+    #     print('train')
+    #     target = deserialize(request.target_array)
+    #     print(target)
+    #     if target is not None:
+    #         return numpy_test_pb2.CompleteReply(complete=1)
+    #     else: return numpy_test_pb2.CompleteReply(complete=-1)
+
+# def deserialize(target):
+#     buf = BytesIO()
+#     buf.write(target)
+#     buf.seek(0)
+#     data_type = buf.read()[:-3]
+#     buf.seek(0)
+
+#     if b'NUMPY' in data_type:
+#         print('in')
+#         return np.load(buf)
+#     else: return -1
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    numpy_test_pb2_grpc.add_Data_Model_HandlerServicer_to_server(Data_model_handler(), server)
+    server.add_insecure_port('[::]:8000')
+    server.start()
+    print('server start')
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
