16461238c0a993a5392c314e43774ed99cd492bb
funkywoong <duswldnd12@naver.com>
2020-07-16 16:36:36 +0900

    Trained model reply complete

--- modi_cloud/modi_cloud.py
+++ modi_cloud/modi_cloud.py
@@ -17,37 +17,20 @@ import util.modi_ai_cloud_pb2_grpc as pb2_grpc
 #example code
 from example.test.test import gen_data, gen_model
 
-# def __parse_data(target):
-#     if isinstance(target, bytes):
-#         return target
-#     buf = BytesIO()
-#     if isinstance(target, np.ndarray):
-#         np.save(buf, target, allow_pickle=True)
-#     elif 'keras' in str(type(target)):
-#         os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-#         from tensorflow.keras.models import save_model
-#         with h5py.File(buf, 'w') as f:
-#             save_model(target, f, include_optimizer=True)
-#     elif 'sklearn' in str(type(target)):
-#         dump(target, buf)
-#     buf.seek(0)
-    
-#     return buf.read()
-
 def run():
 
-    # X_train, X_valid, X_test, y_train, y_valid, y_test = gen_data()
+    X_train, X_valid, X_test, y_train, y_valid, y_test = gen_data()
     model = gen_model()
-    # X_train = __parse_data(X_train)
-    # y_train = __parse_data(y_train)
+    X_train = codec.parse_data(X_train)
+    y_train = codec.parse_data(y_train)
     model = codec.parse_data(model)
-    # print(sys.getsizeof(X_train))
-    # print(sys.getsizeof(y_train))
-    # print(sys.getsizeof(model))
-    tmp_train_list = np.array([1, 2, 3, 4])
-    tmp_train_list = codec.parse_data(tmp_train_list)
-    tmp_label_list = np.array([5, 6, 7, 8])
-    tmp_label_list = codec.parse_data(tmp_label_list)
+    print(sys.getsizeof(X_train))
+    print(sys.getsizeof(y_train))
+    print(sys.getsizeof(model))
+    # tmp_train_list = np.array([1, 2, 3, 4])
+    # tmp_train_list = codec.parse_data(tmp_train_list)
+    # tmp_label_list = np.array([5, 6, 7, 8])
+    # tmp_label_list = codec.parse_data(tmp_label_list)
 
     with grpc.insecure_channel('ec2-15-164-216-238.ap-northeast-2.compute.amazonaws.com:8000') as channel:
         client_stub = pb2_grpc.Data_Model_HandlerStub(channel)
@@ -56,7 +39,7 @@ def run():
         # )
         response_train = client_stub.SendObjects(
             pb2.ObjectsSend(
-                train_array=tmp_train_list, label_array=tmp_label_list, model=model)
+                train_array=X_train, label_array=y_train, model=model)
         )
         print('out request')
     
